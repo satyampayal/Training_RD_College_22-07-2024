e1db0084bda9f06766d82ee92fecb55dc32a3c2a
satyam <satyampayal10@gmail.com>
2024-08-05 10:45:59 +0530

    Arraow function Learn

---
+++ Arraow.js
@@ -0,0 +1,51 @@
+const heroes=(mobile)=>{
+//    const list={
+//     name:"Spider man",
+//     Age:16,
+//     Power:"Swing ",
+//     Team_Work:true,
+//    }
+// let total_salery=0;
+let min_Price=mobile[0].price;
+let emp={
+    name: "Apple",
+    ram: "8Gb",
+    price: 1050,
+};
+mobile.map(m => {
+    // console.log(m.name); 
+    // total_salery+=m.price; 
+    if(min_Price>m.price) {
+        min_Price=m.price;
+        emp.name=m.name;
+        emp.ram=m.ram;
+        emp.price=m.price;
+    }
+});
+
+   console.log(emp)
+}
+
+let mobile=[
+    {
+        name: "samsung",
+        ram: "8Gb",
+        price: 40000,
+      },
+      {
+        name: "Apple",
+        ram: "8Gb",
+        price: 1050,
+      },
+      {
+        name: "vivo",
+        ram: "8Gb",
+        price: 45050,
+      },
+      {
+        name: "Redmi",
+        ram: "8Gb",
+        price: 350,
+      }
+]
+heroes(mobile)
