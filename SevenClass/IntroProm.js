9e757677647de6ad939bd60a7e186d7459d11307
satyam <satyampayal10@gmail.com>
2024-08-14 12:56:01 +0530

    Promice intro

--- SevenClass/IntroProm.js
+++ SevenClass/IntroProm.js
@@ -1,3 +1,18 @@
 // callback hell is complicated to understand because of its structure 
 // there is function pass 
 // now we come to Promices // here is function attached ... and easy to ...
+
+function simpleInterst(p,r,t){
+    let pr=new Promise((res,rej)=>{
+        setTimeout(()=>{
+            if(p>0)
+            res((p*r*t)/100);
+            else rej(" Principle value less then 0")
+        },200)
+    })
+    return pr;
+}
+
+simpleInterst(1,2,3)
+.then((res)=>console.log(res))
+.catch((e)=> console.log(e))
